91eb6e80315f47b8f893af458028db94902a1738
Kurniawan Prasetyo <pras@prime-strategy.co.id>
2018-05-02 15:36:11 +0700

    Fix disqus comment

--- footer.php
+++ footer.php
@@ -47,18 +47,5 @@
 <!-- Load WP Admin Footer -->
 <?php wp_footer(); ?>
 
-<script type="text/javascript">
-  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-  var disqus_shortname = 'dev-moesubs'; // required: replace example with your forum shortname
-
-  /* * * DON'T EDIT BELOW THIS LINE * * */
-  (function () {
-  var s = document.createElement('script'); s.async = true;
-  s.type = 'text/javascript';
-  s.src = 'https://' + disqus_shortname + '.disqus.com/count.js';
-  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-  }());
-</script>
-
 </body>
 </html>
